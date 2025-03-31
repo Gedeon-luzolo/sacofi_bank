b44474ebef5be4557ee2c04165a51841cf8d8990
Gedeon-luzolo <gedeluz9@gmail.com>
2025-03-31 13:24:39 +0100

    Ajout du Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
+import { Toaster } from "sonner";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
         <ErrorBoundary>
           <Suspense fallback={<LoadingSpinner />}>
             {" "}
+            <Toaster position="top-right" richColors />
             <App />
           </Suspense>
         </ErrorBoundary>
