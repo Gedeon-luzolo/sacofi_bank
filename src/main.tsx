cd8704b6033ac9ba4e58d2e66ad9cca9dff6a347
Gedeon-luzolo <gedeluz9@gmail.com>
2025-04-01 21:51:21 +0100

    installation axios et react query

--- src/main.tsx
+++ src/main.tsx
@@ -9,17 +9,23 @@ import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import ErrorBoundary from "./components/errors/ErrorBoundary.tsx";
 import LoadingSpinner from "./components/LoadingSpinner.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <AppWrapper>
         <ErrorBoundary>
-          <Suspense fallback={<LoadingSpinner />}>
-            {" "}
-            <Toaster position="top-right" richColors />
+          <QueryClientProvider client={queryClient}>
             <App />
-          </Suspense>
+            <Suspense fallback={<LoadingSpinner />}>
+              {" "}
+              <Toaster position="top-right" richColors />
+              <App />
+            </Suspense>
+          </QueryClientProvider>
         </ErrorBoundary>
       </AppWrapper>
     </ThemeProvider>
